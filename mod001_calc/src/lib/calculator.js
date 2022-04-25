4539c7b237b3760ff06ecd88177e70f12021eec9
Bruno Pinho <brunopinho@outlook.com>
2022-04-25 17:41:41 -0300

    feat: mod01aula03

--- mod001_calc/src/lib/calculator.js
+++ mod001_calc/src/lib/calculator.js
@@ -1,1 +1,8 @@
-module.exports.sum = (num1, num2) => num1 + num2;
+module.exports.sum = (num1, num2) => {
+  const int1 = parseInt(num1, 10);
+  const int2 = parseInt(num2, 10);
+
+  if (isNaN(int1) || isNaN(int2)) throw new Error('Check your input');
+
+  return int1 + int2;
+};
