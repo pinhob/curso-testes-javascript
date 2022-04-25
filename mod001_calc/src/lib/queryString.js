79e87e053568155e5c00058fe59d14174d9afe2c
Bruno Pinho <brunopinho@outlook.com>
2022-04-25 18:49:25 -0300

    feat: mod01aula07 e mod01aula08

--- mod001_calc/src/lib/queryString.js
+++ mod001_calc/src/lib/queryString.js
@@ -1,3 +1,9 @@
+const keyValueToString = ([key, value]) => {
+  if (typeof (value) === 'object' && !Array.isArray(value)) throw new Error('Check your inputs');
+
+  return `${key}=${value}`
+}
+
 module.exports.queryString = (obj) => Object.entries(obj)
-  .map(([key, value]) => `${key}=${value}`)
+  .map(keyValueToString)
   .join('&');;
--- mod001_calc/src/lib/queryString.spec.js
+++ mod001_calc/src/lib/queryString.spec.js
@@ -9,6 +9,26 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=Bruno&profession=Developer');
   });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Bruno',
+      techs: ['JS', 'Jest'],
+    };
+
+    expect(queryString(obj)).toBe('name=Bruno&techs=JS,Jest');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Bruno',
+      techs: {
+        techs: ['JS', 'Jest'],
+      },
+    };
+
+    expect(() => queryString(obj)).toThrowError();
+  });
 });
 
 describe('Query string to object', () => {
