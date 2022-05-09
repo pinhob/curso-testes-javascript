31a1108932d868b3466d8bfc54d9472c74e12884
Bruno Pinho <brunopinho@outlook.com>
2022-05-09 19:14:54 -0300

    feat: mod01aula21

--- mod001_calc/src/lib/Cart.spec.js
+++ mod001_calc/src/lib/Cart.spec.js
@@ -121,5 +121,18 @@ describe('Cart', () => {
       expect(cart.getTotal().getAmount()).toEqual(26458);
     });
 
+    it('should apply quantity discount for even quantities', () => {
+      const condition = {
+        quantity: 2,
+      }
+
+      cart.add({
+        product,
+        condition,
+        quantity: 4,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(25198);
+    });
   });
 });
