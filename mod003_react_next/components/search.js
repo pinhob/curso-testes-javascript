8ba45add5e4a1ed544cfa7603dbaba2c39b81207
Bruno Pinho <brunopinho@outlook.com>
2022-05-25 15:05:36 -0300

    feat: mod. 03 - aula 04

--- mod003_react_next/components/search.js
+++ mod003_react_next/components/search.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
+
 export default function Search({ doSearch }) {
+  const [term, setTerm] = useState('');
+
   return (
-    <form onSubmit={() => doSearch()} name="search-form" className="relative mt-6 max-w-lg mx-auto">
+    <form onSubmit={() => doSearch(term)} name="search-form" className="relative mt-6 max-w-lg mx-auto">
       <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
         <svg
           className="h-5 w-5 text-gray-500"
@@ -18,8 +22,10 @@ export default function Search({ doSearch }) {
       </span>
 
       <input
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
         className="w-full border rounded-md pl-10 pr-4 py-2 focus:border-blue-500 focus:outline-none focus:shadow-outline"
-        type="text"
+        type="search"
         placeholder="Search"
       />
     </form>
